8eb3142514bdf3f6995be98af9e0304ad5753a5d
Usuario <julian19guerra@gmail.com@DESKTOP-FM3NDOL>
2024-12-15 18:00:29 -0500

    Agregar archivos de proyecto.

---
+++ AlertService.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.JSInterop;
+
+namespace LabScript
+{
+    public class AlertService : IAsyncDisposable, IAlertService
+    {
+
+        readonly Lazy<Task<IJSObjectReference>> ijsObjectReference;
+
+        public AlertService(IJSRuntime ijsruntime)
+        {
+            this.ijsObjectReference = new Lazy<Task<IJSObjectReference>>(() =>
+                ijsruntime.InvokeAsync<IJSObjectReference>("import", "./Home.razor.js").AsTask()
+            );
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (ijsObjectReference.IsValueCreated)
+            {
+                IJSObjectReference moduleJs = await ijsObjectReference.Value;
+                await moduleJs.DisposeAsync();
+            }
+        }
+
+        public async Task CallJsAlertFunction()
+        {
+            var jsModule = await ijsObjectReference.Value;
+            await jsModule.InvokeVoidAsync("jsFuncion");
+        }
+    }
+}
